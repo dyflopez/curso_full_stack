c244b9c1aa2ceff57ef4f17ea60c13163361cd63
Dairis06 <118314903+Dairis06@users.noreply.github.com>
2022-11-22 23:16:10 -0500

    Calculadora
    Operaciones matematicas

---
+++ Dairis_Yinceilys_Nuñez_Rios/Metodos.java
@@ -0,0 +1,13 @@
+public class Metodos {
+    public static void main(String[] args){
+
+        Operaciones op = new Operaciones();
+        op.Bienvenida();
+        op.suma();
+        op.resta();
+        op.multiplicacion();
+        op.division();
+        op.resultado();
+
+    }
+}
