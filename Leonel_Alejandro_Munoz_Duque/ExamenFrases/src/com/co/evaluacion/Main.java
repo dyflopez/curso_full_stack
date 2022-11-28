06f686214832fe2bd45dc9291bfb7d2a025158c3
LeonelAlejandroMunozDuque <lmunozdu@emeal.nttdata.com>
2022-11-28 16:25:32 -0500

    ExamenFrases

---
+++ Leonel_Alejandro_Munoz_Duque/ExamenFrases/src/com/co/evaluacion/Main.java
@@ -0,0 +1,25 @@
+package com.co.evaluacion;
+
+
+import com.co.utilities.Utilies;
+
+import java.awt.*;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Utilies.mostrarMensaje("Ingrese la frase o palabra: ");
+
+        Utilies.Contador();
+
+
+
+
+
+
+
+
+    }
+}
