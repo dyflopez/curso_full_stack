e77490daa0bf52cf5118ddca8e928a620e895f83
Lamudu21 <alejomudu@gmail.com>
2022-12-07 11:40:23 -0500

    Ejercicio Arrays Vectores

---
+++ Leonel_Alejandro_Munoz_Duque/Examen3/untitled/src/EjecutorEjemplo2Arrays.java
@@ -0,0 +1,14 @@
+import supermercado.GestionarSupermercado;
+
+public class EjecutorEjemplo2Arrays {
+
+
+    public static void main(String[] args) {
+
+        GestionarSupermercado prueba = new GestionarSupermercado();
+
+        prueba.menu();
+
+    }
+
+}
