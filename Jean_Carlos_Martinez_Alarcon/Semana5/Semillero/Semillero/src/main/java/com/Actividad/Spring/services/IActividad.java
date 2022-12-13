1fe9675a65b74352d551406eae46df0d23f4abf7
Jcarl10 <jmartala@emeal.nttdata.com>
2022-12-13 07:03:07 -0500

    Spring Ejercicio 2

---
+++ Jean_Carlos_Martinez_Alarcon/Semana5/Semillero/Semillero/src/main/java/com/Actividad/Spring/services/IActividad.java
@@ -0,0 +1,16 @@
+package com.Actividad.Spring.services;
+
+public interface IActividad { // interface, plantilla para la clase, no tiene atributos //
+
+    public String cifrarMensaje(String mensaje);
+
+    public String cifrarConCaracter(String mensaje,String caracter);
+
+
+    public int sumar(int num1, int num2);
+
+    public String invertirCadena(String texto);
+
+
+
+}
